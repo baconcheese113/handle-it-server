a3d3a94712c92291c3e09f12007fc38619a83d38
baconcheese113@hotmail.com <you@example.com>
2021-04-22 03:43:07 -0600

    Log all queries

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,20 @@
 import {ApolloServer} from 'apollo-server'
 import { schema } from './schema'
 import { createContext } from './context'
+import { print } from 'graphql'
 
-const server = new ApolloServer({ schema, context: createContext });
+class BasicLogging {
+    requestDidStart({queryString, parsedQuery, variables}: any) {
+        const query = queryString || print(parsedQuery);
+        console.log(query);
+        console.log(variables);
+    }
+
+    willSendResponse({graphqlResponse}: any) {
+        console.log(JSON.stringify(graphqlResponse, null, 2));
+    }
+}
+
+const server = new ApolloServer({ schema, context: createContext, extensions: [() => new BasicLogging()] });
 
 server.listen(process.env.PORT || 4000).then(({ url }) => { console.log(`🚀  Server ready at ${url}`)})
