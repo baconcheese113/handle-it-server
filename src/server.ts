823a6fde443be893df814066743bd49548c9e9ed
baconcheese113@hotmail.com <you@example.com>
2021-04-22 02:16:10 -0600

    Use heroku's port

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,4 @@ import { createContext } from './context'
 
 const server = new ApolloServer({ schema, context: createContext });
 
-server.listen().then(({ url }) => { console.log(`🚀  Server ready at ${url}`)})
+server.listen(process.env.PORT || 4000).then(({ url }) => { console.log(`🚀  Server ready at ${url}`)})
