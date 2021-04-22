2c1f9b81a1770e4076527fbccf8b8d2a32fbf3a1
baconcheese113@hotmail.com <you@example.com>
2021-04-22 00:54:12 -0600

    init commit with the crudz

---
+++ src/schema.ts
@@ -0,0 +1,78 @@
+import { makeSchema, mutationType, objectType, queryType } from "nexus"
+import path from 'path'
+import { nexusPrisma } from "nexus-plugin-prisma"
+
+const schema = makeSchema({
+    types: [
+        queryType({
+            definition(t) {
+                t.crud.user()
+            }
+        }),
+        mutationType({
+            definition(t) {
+                t.crud.createOneUser()
+                t.crud.createOneHub()
+                t.crud.createOneSensor()
+                t.crud.createOneEvent()
+            }
+        }),
+        objectType({
+            name: 'User',
+            definition(t) {
+                t.model.id()
+                t.model.email()
+                t.model.firstName()
+                t.model.lastName()
+                t.model.hubs()
+            }
+        }),
+        objectType({
+            name: 'Hub',
+            definition(t) {
+                t.model.id()
+                t.model.serial()
+                t.model.batteryLevel()
+                t.model.isCharging()
+                t.model.owner()
+            }
+        }),
+        objectType({
+            name: 'Sensor',
+            definition(t) {
+                t.model.id()
+                t.model.serial()
+                t.model.batteryLevel()
+                t.model.isOpen()
+                t.model.isConnected()
+                t.model.isArmed()
+                t.model.doorColumn()
+                t.model.doorRow()
+                t.model.events()
+            }
+        }),
+        objectType({
+            name: 'Event',
+            definition(t) {
+                t.model.id()
+                t.model.time()
+                t.model.sensor()
+            }
+        })
+    ],
+    plugins: [nexusPrisma({ experimentalCRUD: true })],
+    sourceTypes: {
+        modules: [
+            {
+                module: '@prisma/client',
+                alias: 'prisma'
+            }
+        ]
+    },
+    outputs: {
+        typegen: path.join(process.cwd(), 'generated', 'nexus-typegen.ts'),
+        schema: path.join(process.cwd(), 'generated', 'schema.graphql'),
+    },
+})
+
+export { schema }
---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import {ApolloServer} from 'apollo-server'
+import { schema } from './schema'
+import { createContext } from './context'
+
+const server = new ApolloServer({ schema, context: createContext });
+
+server.listen({ port: 4000 }).then(({ url }) => { console.log(`🚀  Server ready at ${url}`)})
