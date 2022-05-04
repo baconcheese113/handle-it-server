b42dc58a60427166ebf8d09ca30b6ff253d86754
baconcheese113 <baconcheese113@hotmail.com>
2022-05-04 16:36:17 -0600

    Migrate BasicLogging extension to a plugin for Apollo Server v2 -> v3

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,20 @@
 import {ApolloServer} from 'apollo-server'
 import { schema } from './schema'
 import { createContext } from './context'
-import { print } from 'graphql'
 
-class BasicLogging {
-    requestDidStart({queryString, parsedQuery, variables}: any) {
-        const query = queryString || print(parsedQuery);
-        console.log(query);
-        console.log(variables);
+export const server = new ApolloServer({ schema, context: createContext, plugins: [
+    { // BasicLogging plugin
+        async requestDidStart({request}) {
+        console.log("query: ", request.query);
+        console.log("variables: ", request.variables);
+        return {
+            async willSendResponse({response}) {
+                console.log("response: ", JSON.stringify(response, null, 2));
+            },
+            async didEncounterErrors() {
+                console.log("Errors encountered");
+            }
+        }
     }
-
-    willSendResponse({graphqlResponse}: any) {
-        console.log(JSON.stringify(graphqlResponse, null, 2));
-    }
-}
-
-export const server = new ApolloServer({ schema, context: createContext, extensions: [() => new BasicLogging()] })
-
+},
+]})
